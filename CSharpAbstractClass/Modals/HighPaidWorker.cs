537beb55fb201cbd736d3a3721a6e3fdf51f6edc
Gökberk Hayali <gokberkhayali@gmail.com>
2022-01-13 11:11:33 +0100

    Code enhancements.
    Code enhancements.

---
+++ CSharpAbstractClass/Modals/HighPaidWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAbstractClass.Modals
+{
+    public class HighPaidWorker : Worker
+    {
+        public override double CalculateSalary()
+        {
+            return (2000 * 2);
+        }
+    }
+}
---
+++ CSharpAbstractClass/Modals/NormalPaidWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAbstractClass.Modals
+{
+    public class NormalPaidWorker : Worker
+    {
+        public override double CalculateSalary()
+        {
+            return (2000 * 1.5);
+        }
+    }
+}
